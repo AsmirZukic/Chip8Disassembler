c6a35a8ade85c1603dc6a1a0d4a1a449a5540c18
Asmir Zukic <asmir.zukic7@gmail.com>
2023-05-16 20:03:27 +0200

    minor changes, add gitignore file

--- chip8Disassembler.cpp
+++ chip8Disassembler.cpp
@@ -4,10 +4,8 @@
 #include <stdint.h>
 
 
-void logicalOperators(uint8_t lastnib, std::uint8_t *buffer, int pc)
+void logicalOperators(uint8_t lastnib, std::uint8_t *code)
 {
-    std::uint8_t *code = &buffer[pc];
-
     switch(lastnib)
     {
         case 0: printf("%-10s V%01X,V%01X", "MOV.", code[0]&0xf, code[1]>>4); break;
@@ -23,10 +21,8 @@ void logicalOperators(uint8_t lastnib, std::uint8_t *buffer, int pc)
     }
 }
 
-void specialOperations(std::uint8_t *buffer, int pc)
+void specialOperations(std::uint8_t *code)
 {
-    std::uint8_t *code = &buffer[pc];
-
     switch(code[1])
     {
         case 0xe0: printf("%-10s", "CLS"); break;
@@ -35,9 +31,8 @@ void specialOperations(std::uint8_t *buffer, int pc)
     }
 }
 
-void keyboardOperations(std::uint8_t *buffer, int pc)
+void keyboardOperations(std::uint8_t *code)
 {
-    std::uint8_t *code = &buffer[pc];
     switch(code[1])
     {
         case 0x9E: printf("%-10s V%01X", "SKIPKEY.Y", code[0]&0xf); break;
@@ -46,9 +41,8 @@ void keyboardOperations(std::uint8_t *buffer, int pc)
     }
 }
 
-void otherOperations(std::uint8_t *buffer, int pc)
+void otherOperations(std::uint8_t *code)
 {
-    std::uint8_t *code = &buffer[pc];
 
     switch(code[1])
     {
@@ -74,7 +68,7 @@ void DisassembleChip8op(std::uint8_t *buffer, int pc)
 
     switch(firstnib)
     {
-        case 0x00: specialOperations(buffer, pc); break;
+        case 0x00: specialOperations(code); break;
         case 0x01: printf("%-10s $%01x%02x", "JUMP", code[0]&0x0f, code[1]); break;
         case 0x02: printf("%-10s $%01x%02x", "CALL", code[0]&0x0f, code[1]); break;
         case 0x03: printf("%-10s V%01X, #$%02x", "SKIP.EQ", code[0]&0x0f, code[1]); break;
@@ -82,50 +76,43 @@ void DisassembleChip8op(std::uint8_t *buffer, int pc)
         case 0x05: printf("%-10s V%01X, #$%02x", "SKIP.EQ", code[0]&0x0f, code[1]>>4); break;
         case 0x06: printf("%-10s V%01X, #$%02x", "MVI", code[0]&0x0f, code[1]); break;
         case 0x07: printf("%-10s V%01X,#$%02x", "ADI", code[0]&0xf, code[1]); break;
-        case 0x08: logicalOperators(code[1]>>4, buffer, pc); break;
+        case 0x08: logicalOperators(code[1]>>4, code); break;
         case 0x09: printf("%-10s V%01X,V%01X", "SKIP.NE", code[0]&0xf, code[1]>>4); break;
         case 0x0a: printf("%-10s I,#$%01x%02x", "MVI", code[0]&0xf, code[1]); break;
         case 0x0b: printf("%-10s $%01x%02x(V0)", "JUMP", code[0]&0xf, code[1]); break;
 		case 0x0c: printf("%-10s V%01X,#$%02X", "RNDMSK", code[0]&0xf, code[1]); break;
 		case 0x0d: printf("%-10s V%01X,V%01X,#$%01x", "SPRITE", code[0]&0xf, code[1]>>4, code[1]&0xf); break;
-		case 0x0e: keyboardOperations(buffer, pc); break;
-		case 0x0f: otherOperations(buffer, pc); break;
+		case 0x0e: keyboardOperations(code); break;
+		case 0x0f: otherOperations(code); break;
     }
 }
 
 int main( int argc, char* argv[])
 {
-    //Open the file as a stream of binary and move the file pointer to the end
     std::fstream file(argv[1], std::ios::in | std::ios::binary | std::ios::ate );
 
-    //Early termination
     if(!file.is_open())
     {
         std::cout << "Could not open file";
         return 1;
     }
 
-    //Get size of file and alocate a buffer to hold the contents
-    //Memory untill 0x200 is reserved so we add 200 to the size
-    std::streampos size = file.tellg() + (std::streamoff)200;
+    const std::streampos START_ADDRESS = 200;
+    std::streampos size = file.tellg() + START_ADDRESS;
     std::uint8_t* buffer = new std::uint8_t[size];
 
-    //Go back to the beggining of the file and fill the buffer
     file.seekg(0, std::ios::beg);
     file.read((char*)buffer, size);
     file.close();
     
+    int programCounter = 0x200;
 
-    int pc = 0x200;
-
-    while( pc < size)
+    while( programCounter < size)
     {
-        DisassembleChip8op(buffer, pc);
-        pc += 2;
-
+        DisassembleChip8op(buffer, programCounter);
+        programCounter += 2;
         std::cout << std::endl;
     }
-
 
     delete[] buffer;
 
